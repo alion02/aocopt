77b9c3cca8f89df64f0a5ea2162b1ed812587959
alion02 <alojczek@gmail.com>
2024-12-02 03:58:43 +0100

    Implement part 2

--- src/day1.rs
+++ src/day1.rs
@@ -54,7 +54,7 @@ unsafe fn inner1(s: &str) -> impl Display {
             }
         }
 
-        sum += i.abs_diff(j);
+        sum += i.abs_diff(j) as u32;
 
         left[i] -= 1;
         right[j] -= 1;
@@ -64,9 +64,50 @@ unsafe fn inner1(s: &str) -> impl Display {
 }
 
 unsafe fn inner2(s: &str) -> impl Display {
-    todo!();
+    let s = s.as_bytes();
 
-    0
+    let mut left = [0u8; 100000];
+    let mut right = [0u8; 100000];
+
+    for i in (0..).step_by(14).take(1000) {
+        let a = *s.get_unchecked(i + 0) as u32 * 10000
+            + *s.get_unchecked(i + 1) as u32 * 1000
+            + *s.get_unchecked(i + 2) as u32 * 100
+            + *s.get_unchecked(i + 3) as u32 * 10
+            + *s.get_unchecked(i + 4) as u32 * 1
+            - 533328;
+
+        *left.get_unchecked_mut(a as usize) += 1;
+
+        let b = *s.get_unchecked(i + 8) as u32 * 10000
+            + *s.get_unchecked(i + 9) as u32 * 1000
+            + *s.get_unchecked(i + 10) as u32 * 100
+            + *s.get_unchecked(i + 11) as u32 * 10
+            + *s.get_unchecked(i + 12) as u32 * 1
+            - 533328;
+
+        *right.get_unchecked_mut(b as usize) += 1;
+    }
+
+    let mut i = 10000;
+
+    let mut sum = 0u32;
+
+    'outer: loop {
+        while left[i] == 0 {
+            i += 1;
+
+            if i == 100000 {
+                break 'outer;
+            }
+        }
+
+        sum += i as u32 * right[i] as u32;
+
+        left[i] -= 1;
+    }
+
+    sum
 }
 
 pub fn part1(s: &str) -> impl Display {
