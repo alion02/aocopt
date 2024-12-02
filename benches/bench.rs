64f240921aba7dc015cbc3816847c3540d2d1f88
alion02 <alojczek@gmail.com>
2024-12-02 23:52:21 +0100

    silly part 2

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,7 +1,10 @@
 #![feature(portable_simd)]
 
 use std::{
-    arch::x86_64::_mm256_i32gather_epi32, fs::read_to_string, hint::black_box, simd::prelude::*,
+    arch::x86_64::{_mm256_i32gather_epi32, _mm256_loadu_si256},
+    fs::read_to_string,
+    hint::black_box,
+    simd::prelude::*,
 };
 
 use aocopt::day2::{part1, part2};
@@ -27,12 +30,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 pub fn silly(c: &mut Criterion) {
     let a = [0; 256].as_slice().as_ptr();
     c.bench_function("thing", |b| {
-        b.iter(|| unsafe {
-            _mm256_i32gather_epi32::<1>(
-                black_box(a),
-                u32x8::from_array([0, 4, 8, 12, 16, 20, 24, 28]).into(),
-            )
-        })
+        b.iter(|| unsafe { _mm256_loadu_si256(black_box(a as _)) })
     });
 }
 
