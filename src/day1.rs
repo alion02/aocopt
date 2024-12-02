0ca3100fb138a9579d720e41aeaa1ad1e2fbd774
alion02 <alojczek@gmail.com>
2024-12-02 09:44:55 +0100

    Fix out of bounds

--- src/day1.rs
+++ src/day1.rs
@@ -37,7 +37,7 @@ unsafe fn preprocess(s: &str, left: &mut [u8; 90032], right: &mut [u8; 90032]) {
 
     let mut i = 0;
 
-    while i < s.len() {
+    while i < 14000 - 28 {
         let chunk = (s.get_unchecked(i) as *const _ as *const u8x32).read_unaligned();
         let adj_chunk = simd_swizzle!(chunk, [
             00, 01, 02, 03, 04, 05, 06, 07, 08, 09, 10, 11, 12, 13, 13, 13, //
@@ -85,6 +85,81 @@ unsafe fn preprocess(s: &str, left: &mut [u8; 90032], right: &mut [u8; 90032]) {
 
         i += 28;
     }
+
+    let chunk = (s.get_unchecked(i - 4) as *const _ as *const u8x32).read_unaligned();
+    let adj_chunk = simd_swizzle!(chunk, [
+        00 + 4,
+        01 + 4,
+        02 + 4,
+        03 + 4,
+        04 + 4,
+        05 + 4,
+        06 + 4,
+        07 + 4,
+        08 + 4,
+        09 + 4,
+        10 + 4,
+        11 + 4,
+        12 + 4,
+        13 + 4,
+        13 + 4,
+        13 + 4,
+        14 + 4,
+        15 + 4,
+        16 + 4,
+        17 + 4,
+        18 + 4,
+        19 + 4,
+        20 + 4,
+        21 + 4,
+        22 + 4,
+        23 + 4,
+        24 + 4,
+        25 + 4,
+        26 + 4,
+        27 + 4,
+        27 + 4,
+        27 + 4,
+    ]) - u8x32::from_array([
+        b'1', b'0', b'0', b'0', b'0', b'0', b'0', b'0', //
+        b'1', b'0', b'0', b'0', b'0', b'0', b'0', b'0', //
+        b'1', b'0', b'0', b'0', b'0', b'0', b'0', b'0', //
+        b'1', b'0', b'0', b'0', b'0', b'0', b'0', b'0', //
+    ]);
+
+    let s1 = _mm256_maddubs_epi16(
+        adj_chunk.into(),
+        u8x32::from_array([
+            10, 1, 10, 1, 1, 0, 0, 0, 10, 1, 10, 1, 1, 0, 0, 0, //
+            10, 1, 10, 1, 1, 0, 0, 0, 10, 1, 10, 1, 1, 0, 0, 0, //
+        ])
+        .into(),
+    );
+
+    let s2: u16x16 = _mm256_madd_epi16(
+        s1,
+        u16x16::from_array([100, 1, 1, 0, 100, 1, 1, 0, 100, 1, 1, 0, 100, 1, 1, 0]).into(),
+    )
+    .into();
+
+    let s3: __m256i =
+        simd_swizzle!(s2, [0, 2, 4, 6, 0, 2, 4, 6, 8, 10, 12, 14, 8, 10, 12, 14]).into();
+
+    let s4: u32x8 = _mm256_madd_epi16(
+        s3,
+        u16x16::from_array([10, 1, 10, 1, 0, 0, 0, 0, 10, 1, 10, 1, 0, 0, 0, 0]).into(),
+    )
+    .into();
+
+    let a1 = s4[0];
+    let b1 = s4[1];
+    let a2 = s4[4];
+    let b2 = s4[5];
+
+    *left.get_unchecked_mut(a1 as usize) += 1;
+    *left.get_unchecked_mut(a2 as usize) += 1;
+    *right.get_unchecked_mut(b1 as usize) += 1;
+    *right.get_unchecked_mut(b2 as usize) += 1;
 }
 
 #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
