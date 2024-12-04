a99390529df9a26b0df1f08421bb2bbbe19a0d13
alion02 <alojczek@gmail.com>
2024-12-04 01:40:31 +0100

    let's go ub

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::{env::var, fs::File, io::Write, path::PathBuf};
 fn main() {
     println!("cargo::rerun-if-changed=build.rs");
 
-    let mut lut = vec![255u8; 1 << 26].into_boxed_slice();
+    let mut lut = vec![255; 1 << 26].into_boxed_slice();
 
     for (i, vec) in lut.chunks_exact_mut(32).enumerate() {
         let mut mask = (!i & 1) << 1 | i << 2 | 1 << 23;
@@ -40,6 +40,40 @@ fn main() {
 
     let mut path: PathBuf = var("OUT_DIR").unwrap().into();
     path.push("day2lut.bin");
+
+    File::create(path).unwrap().write_all(&lut).unwrap();
+
+    let mut lut = vec![255; 1 << 20].into_boxed_slice();
+
+    for (i, vec) in lut.chunks_exact_mut(16).enumerate() {
+        let mut mask = (i >> 4 & 0x7F) ^ 0xFF;
+        let first_len = mask.trailing_zeros() as usize;
+        mask &= mask - 1;
+        mask >>= first_len + 1;
+        let second_len = mask.trailing_zeros() as usize;
+        if !(1..=3).contains(&first_len) || !(1..=3).contains(&second_len) {
+            continue;
+        }
+        let first_num = 4;
+        let comma = first_num + first_len;
+        let second_num = comma + 1;
+        let close = second_num + second_len;
+        for i in 0..first_len {
+            vec[3 - first_len + i] = i as u8 + first_num as u8;
+        }
+        for i in 0..second_len {
+            vec[4 + 3 - second_len + i] = i as u8 + second_num as u8;
+        }
+        vec[8] = 0;
+        vec[9] = 1;
+        vec[10] = 2;
+        vec[11] = 3;
+        vec[12] = comma as u8;
+        vec[13] = close as u8;
+    }
+
+    let mut path: PathBuf = var("OUT_DIR").unwrap().into();
+    path.push("day3lut.bin");
 
     File::create(path).unwrap().write_all(&lut).unwrap();
 }
--- src/day3.rs
+++ src/day3.rs
@@ -1,8 +1,56 @@
+use std::arch::x86_64::{_mm_hadd_epi16, _mm_shuffle_epi8};
+
 use super::*;
+
+static LUT: [u8x16; 1 << 16] =
+    unsafe { transmute(*include_bytes!(concat!(env!("OUT_DIR"), "/day3lut.bin"))) };
 
 #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
 unsafe fn inner1(s: &str) -> u32 {
-    0
+    let r = s.as_bytes().as_ptr_range();
+    let mut ptr = r.start;
+    let end = r.end;
+    let lut = &LUT;
+    let mut sum = 0;
+    'chunk: loop {
+        let chunk = (ptr as *const u8x32).read_unaligned();
+        let is_u = chunk.simd_eq(Simd::splat(b'u'));
+        let mut u_mask = is_u.to_bitmask() as u32;
+        loop {
+            let u_offset = u_mask.trailing_zeros();
+            if u_offset == 32 {
+                ptr = ptr.add(32);
+                if ptr < end {
+                    continue 'chunk;
+                }
+                return sum;
+            }
+            let instruction = (ptr.add(u_offset as _).sub(1) as *const u8x16).read_unaligned();
+            let normalized = instruction - Simd::splat(b'0');
+            let is_digit = normalized.simd_lt(Simd::splat(10));
+            let digit_mask = is_digit.to_bitmask() as u32;
+            let shuffle_idx = *lut.get_unchecked(digit_mask as usize);
+            let discombobulated: i8x16 =
+                _mm_shuffle_epi8(normalized.into(), shuffle_idx.into()).into();
+            let is_correct = discombobulated.simd_eq(Simd::from_array([
+                0, 0, 0, 0, 0, 0, 0, 0, 61, 69, 60, -8, -4, -7, 0, 0,
+            ])) | Mask::from_array([
+                true, true, true, true, true, true, true, true, //
+                false, false, false, false, false, false, true, true, //
+            ]);
+            u_mask &= u_mask - 1;
+            if !is_correct.all() {
+                continue;
+            }
+            let two_digit = _mm_maddubs_epi16(
+                discombobulated.into(),
+                u8x16::from_array([100, 10, 1, 0, 100, 10, 1, 0, 100, 10, 1, 0, 100, 10, 1, 0])
+                    .into(),
+            );
+            let three_digit: u16x8 = _mm_hadd_epi16(two_digit, two_digit).into();
+            sum += three_digit[0] as u32 * three_digit[1] as u32;
+        }
+    }
 }
 
 #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
