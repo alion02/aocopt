b097142f9c77d2805be006aeacfebeb569dddd46
alion02 <alojczek@gmail.com>
2024-12-06 23:36:47 +0100

    Bench the wip

--- src/day6.rs
+++ src/day6.rs
@@ -1,3 +1,5 @@
+use std::hint::black_box;
+
 use super::*;
 
 static mut SCRATCH: [u8x32; 67] = [u8x32::from_array([0; 32]); 67];
@@ -96,6 +98,56 @@ unsafe fn inner1(s: &[u8]) -> u32 {
 
 #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
 unsafe fn inner2(s: &[u8]) -> u32 {
+    let mut up_lo: [u64; 130] = std::array::from_fn(|i| {
+        (0..64).fold(0, |acc, j| {
+            acc | ((*s.get_unchecked((i) + (127 - j) * 131) == b'#') as u64) << j
+        })
+    });
+    let mut up_hi: [u64; 130] = std::array::from_fn(|i| {
+        (0..64).fold(0, |acc, j| {
+            acc | ((*s.get_unchecked((i) + (63 - j) * 131) == b'#') as u64) << j
+        })
+    });
+    let mut right_lo: [u64; 130] = std::array::from_fn(|i| {
+        (0..64).fold(0, |acc, j| {
+            acc | ((*s.get_unchecked((2 + j) + (i) * 131) == b'#') as u64) << j
+        })
+    });
+    let mut right_hi: [u64; 130] = std::array::from_fn(|i| {
+        (0..64).fold(0, |acc, j| {
+            acc | ((*s.get_unchecked((66 + j) + (i) * 131) == b'#') as u64) << j
+        })
+    });
+    let mut down_lo: [u64; 130] = std::array::from_fn(|i| {
+        (0..64).fold(0, |acc, j| {
+            acc | ((*s.get_unchecked((i) + (2 + j) * 131) == b'#') as u64) << j
+        })
+    });
+    let mut down_hi: [u64; 130] = std::array::from_fn(|i| {
+        (0..64).fold(0, |acc, j| {
+            acc | ((*s.get_unchecked((i) + (66 + j) * 131) == b'#') as u64) << j
+        })
+    });
+    let mut left_lo: [u64; 130] = std::array::from_fn(|i| {
+        (0..64).fold(0, |acc, j| {
+            acc | ((*s.get_unchecked((127 - j) + (i) * 131) == b'#') as u64) << j
+        })
+    });
+    let mut left_hi: [u64; 130] = std::array::from_fn(|i| {
+        (0..64).fold(0, |acc, j| {
+            acc | ((*s.get_unchecked((63 - j) + (i) * 131) == b'#') as u64) << j
+        })
+    });
+    black_box((
+        &mut up_lo,
+        &mut up_hi,
+        &mut right_lo,
+        &mut right_hi,
+        &mut down_lo,
+        &mut down_hi,
+        &mut left_lo,
+        &mut left_hi,
+    ));
     0
 }
 
