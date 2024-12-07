771abf0737d32baeebf094700fe93f516985bc21
alion02 <alojczek@gmail.com>
2024-12-08 00:23:56 +0100

    done

--- src/day7.rs
+++ src/day7.rs
@@ -1,13 +1,93 @@
 use super::*;
 
+// list len 3-12
+// list number range 1-999
+// len < 5 uncommon
+// target digit count range 2?-15?
+
 #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
 #[allow(unreachable_code)]
-unsafe fn inner1(s: &[u8]) -> u32 {
-    0
+unsafe fn inner1(s: &[u8]) -> u64 {
+    let r = s.as_ptr_range();
+    let mut ptr = r.start;
+    let mut total = 0;
+    let mut numbers = [MaybeUninit::<u16>::uninit(); 12];
+    loop {
+        let mut target = *ptr as u64 - b'0' as u64;
+        ptr = ptr.add(1);
+        loop {
+            let b = *ptr;
+            if b == b':' {
+                ptr = ptr.add(2);
+                break;
+            }
+            ptr = ptr.add(1);
+            target *= 10;
+            target += b as u64 - b'0' as u64;
+        }
+
+        let list_end = numbers.as_mut_ptr().cast::<u16>();
+        let mut list = list_end;
+        'outer: loop {
+            let mut num = *ptr as i32 - b'0' as i32;
+            ptr = ptr.add(1);
+            loop {
+                let b = *ptr as i32 - b'0' as i32;
+                ptr = ptr.add(1);
+                if b < 0 {
+                    list.write(num as u16);
+                    if b == b'\n' as i32 - b'0' as i32 {
+                        break 'outer;
+                    }
+                    list = list.add(1);
+                    break;
+                }
+                num *= 10;
+                num += b;
+            }
+        }
+
+        #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
+        unsafe fn f(target: u64, list: *mut u16, list_end: *mut u16) -> bool {
+            let curr = *list as u64;
+            if list == list_end {
+                return target == curr;
+            }
+            target % curr == 0 && f(target / curr, list.sub(1), list_end)
+                || target > curr && f(target - curr, list.sub(1), list_end)
+        }
+
+        if f(target, list, list_end) {
+            total += target;
+        }
+
+        if ptr == r.end {
+            break;
+        }
+
+        // asm!(
+        // "20:",
+        //     "movzx cx, [{list}]",
+        //     "mov rbx, rax",
+        //     "div rcx",
+        //     "test rdx, rdx",
+        //     "jnz 21f",
+        //     "",
+        //     "/*{list} {end}*/",
+        //     inout("rax") target => _,
+        //     out("rdx") _,
+        //     out("rcx") _,
+        //     out("rbx") _,
+        //     list = inout(reg) list => _,
+        //     end = in(reg) list_end,
+        // );
+    }
+
+    total
 }
 
 #[target_feature(enable = "avx2,bmi1,bmi2,cmpxchg16b,lzcnt,movbe,popcnt")]
-unsafe fn inner2(s: &[u8]) -> u32 {
+unsafe fn inner2(s: &[u8]) -> u64 {
     0
 }
 
