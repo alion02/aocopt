b03e4c4b41c66796c6327042a32487c9cc45b663
alion02 <alojczek@gmail.com>
2024-12-07 04:37:05 +0100

    refactor

--- src/day6.rs
+++ src/day6.rs
@@ -135,49 +135,56 @@ unsafe fn inner2(s: &[u8]) -> u32 {
         options(nostack, readonly),
     );
 
-    let up_lo: [u64; 130] = std::array::from_fn(|i| {
-        (0..64).fold(0, |acc, j| {
-            acc | ((*s.get_unchecked((i) + (127 - j) * 131) == b'#') as u64) << j
-        })
-    });
-    let up_hi: [u64; 130] = std::array::from_fn(|i| {
-        (0..64).fold(0, |acc, j| {
-            acc | ((*s.get_unchecked((i) + (63 - j) * 131) == b'#') as u64) << j
-        })
-    });
-    let right_lo: [u64; 130] = std::array::from_fn(|i| {
-        (0..64).fold(0, |acc, j| {
-            acc | ((*s.get_unchecked((2 + j) + (i) * 131) == b'#') as u64) << j
-        })
-    });
-    let right_hi: [u64; 130] = std::array::from_fn(|i| {
-        (0..64).fold(0, |acc, j| {
-            acc | ((*s.get_unchecked((66 + j) + (i) * 131) == b'#') as u64) << j
-        })
-    });
-    let down_lo: [u64; 130] = std::array::from_fn(|i| {
-        (0..64).fold(0, |acc, j| {
-            acc | ((*s.get_unchecked((i) + (2 + j) * 131) == b'#') as u64) << j
-        })
-    });
-    let down_hi: [u64; 130] = std::array::from_fn(|i| {
-        (0..64).fold(0, |acc, j| {
-            acc | ((*s.get_unchecked((i) + (66 + j) * 131) == b'#') as u64) << j
-        })
-    });
-    let left_lo: [u64; 130] = std::array::from_fn(|i| {
-        (0..64).fold(0, |acc, j| {
-            acc | ((*s.get_unchecked((127 - j) + (i) * 131) == b'#') as u64) << j
-        })
-    });
-    let left_hi: [u64; 130] = std::array::from_fn(|i| {
-        (0..64).fold(0, |acc, j| {
-            acc | ((*s.get_unchecked((63 - j) + (i) * 131) == b'#') as u64) << j
-        })
-    });
+    struct Tables {
+        obstacles: [[u64; 130]; 8],
+        visisted: [[bool; 130]; 130],
+    }
 
-    let visited = &mut SCRATCH;
-    visited.fill(Simd::splat(0));
+    let tables = Tables {
+        obstacles: [
+            std::array::from_fn(|i| {
+                (0..64).fold(0, |acc, j| {
+                    acc | ((*s.get_unchecked((i) + (127 - j) * 131) == b'#') as u64) << j
+                })
+            }),
+            std::array::from_fn(|i| {
+                (0..64).fold(0, |acc, j| {
+                    acc | ((*s.get_unchecked((i) + (63 - j) * 131) == b'#') as u64) << j
+                })
+            }),
+            std::array::from_fn(|i| {
+                (0..64).fold(0, |acc, j| {
+                    acc | ((*s.get_unchecked((2 + j) + (i) * 131) == b'#') as u64) << j
+                })
+            }),
+            std::array::from_fn(|i| {
+                (0..64).fold(0, |acc, j| {
+                    acc | ((*s.get_unchecked((66 + j) + (i) * 131) == b'#') as u64) << j
+                })
+            }),
+            std::array::from_fn(|i| {
+                (0..64).fold(0, |acc, j| {
+                    acc | ((*s.get_unchecked((i) + (2 + j) * 131) == b'#') as u64) << j
+                })
+            }),
+            std::array::from_fn(|i| {
+                (0..64).fold(0, |acc, j| {
+                    acc | ((*s.get_unchecked((i) + (66 + j) * 131) == b'#') as u64) << j
+                })
+            }),
+            std::array::from_fn(|i| {
+                (0..64).fold(0, |acc, j| {
+                    acc | ((*s.get_unchecked((127 - j) + (i) * 131) == b'#') as u64) << j
+                })
+            }),
+            std::array::from_fn(|i| {
+                (0..64).fold(0, |acc, j| {
+                    acc | ((*s.get_unchecked((63 - j) + (i) * 131) == b'#') as u64) << j
+                })
+            }),
+        ],
+        visisted: [[false; 130]; 130],
+    };
 
     let masks = &MASKS;
 
@@ -187,8 +194,8 @@ unsafe fn inner2(s: &[u8]) -> u32 {
     let mut y = loc / 131;
 
     loop {
-        let near = up_lo.get_unchecked(x) & masks.get_unchecked(192 - y);
-        let far = up_hi.get_unchecked(x) & masks.get_unchecked(128 - y);
+        let near = tables.obstacles[0].get_unchecked(x) & masks.get_unchecked(192 - y);
+        let far = tables.obstacles[1].get_unchecked(x) & masks.get_unchecked(128 - y);
         if near | far == 0 {
             break;
         }
@@ -197,8 +204,8 @@ unsafe fn inner2(s: &[u8]) -> u32 {
         let c = if c_lo == 64 { c_hi } else { c_lo };
         y = 128 - c as usize;
 
-        let near = right_lo.get_unchecked(y) & masks.get_unchecked(x + 63);
-        let far = right_hi.get_unchecked(y) & masks.get_unchecked(x - 1);
+        let near = tables.obstacles[2].get_unchecked(y) & masks.get_unchecked(x + 63);
+        let far = tables.obstacles[3].get_unchecked(y) & masks.get_unchecked(x - 1);
         if near | far == 0 {
             break;
         }
@@ -207,8 +214,8 @@ unsafe fn inner2(s: &[u8]) -> u32 {
         let c = if c_lo == 64 { c_hi } else { c_lo };
         x = c as usize + 1;
 
-        let near = down_lo.get_unchecked(x) & masks.get_unchecked(y + 63);
-        let far = down_hi.get_unchecked(x) & masks.get_unchecked(y - 1);
+        let near = tables.obstacles[4].get_unchecked(x) & masks.get_unchecked(y + 63);
+        let far = tables.obstacles[5].get_unchecked(x) & masks.get_unchecked(y - 1);
         if near | far == 0 {
             break;
         }
@@ -217,8 +224,8 @@ unsafe fn inner2(s: &[u8]) -> u32 {
         let c = if c_lo == 64 { c_hi } else { c_lo };
         y = c as usize + 1;
 
-        let near = left_lo.get_unchecked(y) & masks.get_unchecked(192 - x);
-        let far = left_hi.get_unchecked(y) & masks.get_unchecked(128 - x);
+        let near = tables.obstacles[6].get_unchecked(y) & masks.get_unchecked(192 - x);
+        let far = tables.obstacles[7].get_unchecked(y) & masks.get_unchecked(128 - x);
         if near | far == 0 {
             break;
         }
