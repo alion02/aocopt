3c01b2fe05563797af9e71511ad201343c1476eb
alion02 <alojczek@gmail.com>
2024-12-08 05:14:54 +0100

    reorder for ease of copypasta

--- src/lib.rs
+++ src/lib.rs
@@ -8,20 +8,6 @@
     clippy::zero_prefixed_literal
 )]
 
-#[macro_use]
-extern crate aoc_runner_derive;
-extern crate aoc_runner;
-
-pub mod day1;
-pub mod day2;
-pub mod day3;
-pub mod day4;
-pub mod day5;
-pub mod day6;
-pub mod day7;
-
-aoc_lib! { year = 2024 }
-
 use std::{
     arch::{
         asm,
@@ -35,3 +21,17 @@ use std::{
     mem::{transmute, MaybeUninit},
     simd::prelude::*,
 };
+
+#[macro_use]
+extern crate aoc_runner_derive;
+extern crate aoc_runner;
+
+pub mod day1;
+pub mod day2;
+pub mod day3;
+pub mod day4;
+pub mod day5;
+pub mod day6;
+pub mod day7;
+
+aoc_lib! { year = 2024 }
