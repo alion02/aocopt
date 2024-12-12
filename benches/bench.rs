30259bcd80e16a6abbefacc8d754aa3fe5637133
alion02 <alojczek@gmail.com>
2024-12-12 08:15:19 +0100

    now featuring testing

--- benches/bench.rs
+++ benches/bench.rs
@@ -12,14 +12,8 @@ pub fn day11(c: &mut Criterion) {
     c.bench_function("day11 part1", |b| b.iter(|| part1(black_box(s))));
     c.bench_function("day11 part2", |b| b.iter(|| part2(black_box(s))));
 
-    // assert_eq!(
-    //     part1(s).to_string(),
-    //     read_to_string("./outputs/11p1.txt").unwrap(),
-    // );
-    // assert_eq!(
-    //     part2(s).to_string(),
-    //     read_to_string("./outputs/11p2.txt").unwrap(),
-    // );
+    assert_eq!(part1(s).to_string(), read_to_string("./outputs/11p1.txt").unwrap());
+    assert_eq!(part2(s).to_string(), read_to_string("./outputs/11p2.txt").unwrap());
 }
 
 criterion_group!(benches, day11);
