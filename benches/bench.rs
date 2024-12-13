907af5dc118fecdcc2c4c5c3b9c4e5fe8943003c
alion02 <alojczek@gmail.com>
2024-12-13 09:26:08 +0100

    ahhh the bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -10,10 +10,10 @@ pub fn day12(c: &mut Criterion) {
     let s = s.as_str();
 
     c.bench_function("day12 part1", |b| b.iter(|| part1(black_box(s))));
-    // c.bench_function("day12 part2", |b| b.iter(|| part2(black_box(s))));
+    c.bench_function("day12 part2", |b| b.iter(|| part2(black_box(s))));
 
     assert_eq!(part1(s).to_string(), read_to_string("./outputs/12p1.txt").unwrap());
-    // assert_eq!(part2(s).to_string(), read_to_string("./outputs/12p2.txt").unwrap());
+    assert_eq!(part2(s).to_string(), read_to_string("./outputs/12p2.txt").unwrap());
 }
 
 criterion_group!(benches, day12);
