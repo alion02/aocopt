6044847552ca6b2320db2b457f6336ae92613ee7
alion02 <alojczek@gmail.com>
2024-12-15 22:24:30 +0100

    unreorder

--- src/day15.rs
+++ src/day15.rs
@@ -40,8 +40,8 @@ unsafe fn inner1(s: &[u8]) -> u32 {
     // O repeats
         "add {block_pos:e}, dword ptr[{dir_table} + {inst} * 2]",
         "cmp byte ptr[{map} + {block_pos}], 46",
-        "jb 21b",
         "ja 22b",
+        "jb 21b",
     // O then .
     "23:",
         "mov byte ptr[{map} + {pos}], 46",
