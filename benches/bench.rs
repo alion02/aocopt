40de1c3c159344eeef8bbcd4f6b285380b354c9a
alion02 <alojczek@gmail.com>
2024-12-17 09:40:46 +0100

    bench It:tm:

--- benches/bench.rs
+++ benches/bench.rs
@@ -10,10 +10,10 @@ pub fn day16(c: &mut Criterion) {
     let s = s.as_str();
 
     c.bench_function("day16 part1", |b| b.iter(|| part1(black_box(s))));
-    // c.bench_function("day16 part2", |b| b.iter(|| part2(black_box(s))));
+    c.bench_function("day16 part2", |b| b.iter(|| part2(black_box(s))));
 
     assert_eq!(part1(s).to_string(), read_to_string("./outputs/16p1.txt").unwrap());
-    // assert_eq!(part2(s).to_string(), read_to_string("./outputs/16p2.txt").unwrap());
+    assert_eq!(part2(s).to_string(), read_to_string("./outputs/16p2.txt").unwrap());
 }
 
 criterion_group!(benches, day16);
