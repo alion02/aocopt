3f275fe8238e3421e794b06d7a93e4511a8abbb7
alion02 <alojczek@gmail.com>
2024-12-18 00:17:51 +0100

    solve p1

--- src/day17.rs
+++ src/day17.rs
@@ -2,7 +2,44 @@ use super::*;
 
 #[inline]
 unsafe fn inner1(s: &[u8]) -> &str {
-    ""
+    static mut BUF: [u8; 17] = [b','; 17];
+
+    let chunk = s.as_ptr().add(12).cast::<u8x16>().read_unaligned();
+    let chunk = chunk - Simd::splat(b'0');
+    let chunk = _mm_maddubs_epi16(
+        chunk.into(),
+        u8x16::from_array([10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1, 10, 1]).into(),
+    );
+    let chunk = _mm_madd_epi16(chunk, u16x8::from_array([100, 1, 100, 1, 100, 1, 100, 1]).into());
+    let chunk = _mm_packus_epi32(chunk, chunk);
+    let chunk = _mm_madd_epi16(
+        chunk,
+        u16x8::from_array([10000, 1, 10000, 1, 10000, 1, 10000, 1]).into(),
+    );
+    let mut a = u32x4::from(chunk)[0];
+    let imm1 = *s.get_unchecked(65) as u32 - b'0' as u32;
+    let chunk = s.as_ptr().add(64).cast::<u8x16>().read_unaligned();
+    let chunk = chunk.simd_eq(Simd::from_array([
+        0, 0, 0, b'1', 0, 0, 0, b'1', 0, 0, 0, b'1', 0, 0, 0, b'1',
+    ]));
+    let mask = chunk.to_bitmask() as u32;
+    let offset = mask.trailing_zeros() as usize;
+
+    let imm2 = *s.get_unchecked(64 + offset + 2) as u32 - b'0' as u32;
+
+    let buf = &mut BUF;
+    let mut len = s.len();
+    loop {
+        let b = a % 8 ^ imm1;
+        *buf.get_unchecked_mut(len - 91) = ((a >> b ^ b ^ imm2) % 8 + b'0' as u32) as u8;
+        a >>= 3;
+        len += 2;
+        if a == 0 {
+            break;
+        }
+    }
+
+    std::str::from_utf8_unchecked(buf)
 }
 
 #[inline]
