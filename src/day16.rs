934a1598ba1fd89f22fd1dfe815e564a8b70f359
alion02 <alojczek@gmail.com>
2024-12-17 04:50:39 +0100

    make it quite fast

--- src/day16.rs
+++ src/day16.rs
@@ -1,5 +1,3 @@
-use std::collections::VecDeque;
-
 use super::*;
 
 macro_rules! row_len {
@@ -22,128 +20,100 @@ macro_rules! far_edge {
 
 #[inline]
 unsafe fn inner1(s: &[u8]) -> u32 {
+    static mut CURR: [Node; 65536] = [unsafe { transmute(0) }; 65536];
+    static mut NEXT: [Node; 65536] = [unsafe { transmute(0) }; 65536];
+    static OFFSET: [i16; 4] = [1, row_len!(), -1, -row_len!()];
+
+    let mut visited = [0u8; row_len!() * side_len!()];
+    let mut curr = &mut CURR;
+    let mut next = &mut NEXT;
+    let offset = &OFFSET;
+
     #[derive(Clone, Copy)]
+    #[repr(align(4))]
     struct Node {
         pos: u16,
         dir: u8,
         cost: u8,
     }
 
-    let mut curr = VecDeque::<Node>::from_iter([Node {
+    curr[0] = Node {
         pos: far_edge!() * row_len!() + 1,
         dir: 0,
         cost: 0,
-    }]);
-    let mut next = VecDeque::<Node>::new();
-
-    let mut front = VecDeque::<Node>::new();
+    };
+    curr[1].cost = !0;
 
     let mut turn_cost = 0;
-
-    let mut visited = [0u8; row_len!() * side_len!()];
-
     loop {
-        // let mut map = s.to_vec();
-        // for node in &front {
-        //     map[node.pos as usize] = b">v<^"[node.dir as usize];
-        // }
-        // for node in &curr {
-        //     map[node.pos as usize] = b'*';
-        // }
-        // for node in &next {
-        //     map[node.pos as usize] = b'+';
-        // }
-        // for y in 0..side_len!() {
-        //     println!(
-        //         "{}",
-        //         std::str::from_utf8(&map[y * row_len!()..y * row_len!() + side_len!()]).unwrap()
-        //     );
-        // }
+        let mut i = 0;
+        let mut k = 0;
+        loop {
+            let mut j = i;
+            let cost = curr.get_unchecked_mut(j).cost;
+            let next_cost = cost + 1;
+            loop {
+                let node = curr.get_unchecked_mut(j);
+                let mut pos = node.pos;
+                assert!(*s.get_unchecked(pos as usize) != b'#');
+                if pos == row_len!() + far_edge!() {
+                    return turn_cost + cost as u32 * 2;
+                }
+                let mut dir = node.dir;
+                let visit_mask = dir & 1;
+                'delete: {
+                    if *visited.get_unchecked(pos as usize) & visit_mask == 0 {
+                        *visited.get_unchecked_mut(pos as usize) |= visit_mask;
+                        dir ^= 1;
+                        {
+                            let pos = pos.wrapping_add_signed(*offset.get_unchecked(dir as usize));
+                            if *s.get_unchecked(pos as usize) != b'#' {
+                                *next.get_unchecked_mut(k) = Node {
+                                    pos: pos.wrapping_add_signed(*offset.get_unchecked(dir as usize)),
+                                    dir,
+                                    cost: next_cost,
+                                };
+                                k += 1;
+                            }
+                        }
+                        dir ^= 2;
+                        {
+                            let pos = pos.wrapping_add_signed(*offset.get_unchecked(dir as usize));
+                            if *s.get_unchecked(pos as usize) != b'#' {
+                                *next.get_unchecked_mut(k) = Node {
+                                    pos: pos.wrapping_add_signed(*offset.get_unchecked(dir as usize)),
+                                    dir,
+                                    cost: next_cost,
+                                };
+                                k += 1;
+                            }
+                        }
+                        dir ^= 3;
+                        pos = pos.wrapping_add_signed(*offset.get_unchecked(dir as usize));
+                        if *s.get_unchecked(pos as usize) != b'#' {
+                            node.pos = pos.wrapping_add_signed(*offset.get_unchecked(dir as usize));
+                            node.cost = next_cost;
+                            break 'delete;
+                        }
+                    }
+                    *curr.get_unchecked_mut(j) = *curr.get_unchecked(i);
+                    i += 1;
+                }
 
-        if front.is_empty() {
-            if curr.is_empty() {
-                (curr, next) = (next, curr);
-                turn_cost += 1000;
+                j += 1;
+                if curr.get_unchecked(j).cost > cost {
+                    break;
+                }
             }
 
-            front.push_back(curr.pop_front().unwrap_unchecked());
-        }
-
-        while let Some(node) = curr.front() {
-            if node.cost != front.front().unwrap_unchecked().cost {
+            if curr.get_unchecked(i).cost == !0 {
                 break;
             }
-            front.push_back(curr.pop_front().unwrap_unchecked());
         }
 
-        let mut found_end = false;
-        front.retain_mut(
-            |&mut Node {
-                 pos: ref mut pos_ref,
-                 dir,
-                 cost: ref mut cost_ref,
-             }| {
-                if found_end {
-                    return false;
-                }
-
-                let pos = *pos_ref;
-                let cost = *cost_ref + 1;
-
-                if pos == row_len!() + far_edge!() {
-                    found_end = true;
-                    return true;
-                }
-
-                macro_rules! offset {
-                    ($dir:expr) => {
-                        *[1i16, row_len!(), -1, -row_len!()].get_unchecked($dir as usize)
-                    };
-                }
-
-                let off = offset!(dir);
-                let mut npos = pos.wrapping_add_signed(off);
-                let retain = if *s.get_unchecked(npos as usize) != b'#' && {
-                    npos = npos.wrapping_add_signed(off);
-                    *visited.get_unchecked(npos as usize) & 5 << (dir & 1) == 0
-                } {
-                    *visited.get_unchecked_mut(npos as usize) |= 1 << dir;
-                    *cost_ref = cost;
-                    *pos_ref = npos;
-                    true
-                } else {
-                    false
-                };
-
-                let dir = dir ^ 1;
-                let off = offset!(dir);
-                let mut npos = pos.wrapping_add_signed(off);
-                if *s.get_unchecked(npos as usize) != b'#' && {
-                    npos = npos.wrapping_add_signed(off);
-                    *visited.get_unchecked(npos as usize) & 5 << (dir & 1) == 0
-                } {
-                    *visited.get_unchecked_mut(npos as usize) |= 1 << dir;
-                    next.push_back(Node { pos: npos, dir, cost });
-                }
-
-                let dir = dir ^ 2;
-                let off = offset!(dir);
-                let mut npos = pos.wrapping_add_signed(off);
-                if *s.get_unchecked(npos as usize) != b'#' && {
-                    npos = npos.wrapping_add_signed(off);
-                    *visited.get_unchecked(npos as usize) & 5 << (dir & 1) == 0
-                } {
-                    *visited.get_unchecked_mut(npos as usize) |= 1 << dir;
-                    next.push_back(Node { pos: npos, dir, cost });
-                }
-
-                retain
-            },
-        );
-
-        if found_end {
-            return turn_cost + front.back().unwrap_unchecked().cost as u32 * 2;
-        }
+        turn_cost += 1000;
+        (curr, next) = (next, curr);
+        curr.get_unchecked_mut(k).cost = !0;
     }
 }
 
