aa31b53198f2c93645d69b85d66bedd116d1f133
alion02 <alojczek@gmail.com>
2024-12-17 22:37:17 +0100

    Prepare day 17

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,19 +2,19 @@
 
 use std::{fs::read_to_string, hint::black_box};
 
-use aocopt::day16::{part1, part2};
+use aocopt::day17::{part1, part2};
 use criterion::{criterion_group, criterion_main, Criterion};
 
-pub fn day16(c: &mut Criterion) {
-    let s = read_to_string("./inputs/16.txt").unwrap();
+pub fn day17(c: &mut Criterion) {
+    let s = read_to_string("./inputs/17.txt").unwrap();
     let s = s.as_str();
 
-    c.bench_function("day16 part1", |b| b.iter(|| part1(black_box(s))));
-    c.bench_function("day16 part2", |b| b.iter(|| part2(black_box(s))));
+    c.bench_function("day17 part1", |b| b.iter(|| part1(black_box(s))));
+    c.bench_function("day17 part2", |b| b.iter(|| part2(black_box(s))));
 
-    assert_eq!(part1(s).to_string(), read_to_string("./outputs/16p1.txt").unwrap());
-    assert_eq!(part2(s).to_string(), read_to_string("./outputs/16p2.txt").unwrap());
+    assert_eq!(part1(s).to_string(), read_to_string("./outputs/17p1.txt").unwrap());
+    assert_eq!(part2(s).to_string(), read_to_string("./outputs/17p2.txt").unwrap());
 }
 
-criterion_group!(benches, day16);
+criterion_group!(benches, day17);
 criterion_main!(benches);
