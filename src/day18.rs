70379e4d89fdccda11092988920839d3838c20c4
alion02 <alojczek@gmail.com>
2024-12-19 05:13:25 +0100

    crank chunks of 4

--- src/day18.rs
+++ src/day18.rs
@@ -43,6 +43,45 @@ static LUT: [i8x16; 512] = unsafe {
 
 #[inline]
 unsafe fn inner1(s: &[u8]) -> u32 {
+    static LUT: [i8x16; 512] = unsafe {
+        let mut lut = [[-1i8; 16]; 512];
+
+        let mut idx = 0;
+        while idx < 512 {
+            let shuffle = &mut lut[idx];
+
+            let mut mask = idx << 2;
+            if idx & 1 == 0 {
+                mask |= 2;
+            }
+            mask |= 0x800;
+
+            let mut slot = 0;
+            let mut byte = 0;
+            while slot < 16 {
+                let zeros = mask.trailing_zeros();
+                match zeros {
+                    1 => {
+                        shuffle[slot + 2] = byte;
+                        byte += 2;
+                    }
+                    2 => {
+                        shuffle[slot] = byte;
+                        shuffle[slot + 2] = byte + 1;
+                        byte += 3;
+                    }
+                    _ => break,
+                }
+                mask >>= zeros + 1;
+                slot += 4;
+            }
+
+            idx += 1;
+        }
+
+        transmute(lut)
+    };
+
     let mut ptr = s.as_ptr().cast::<i8x16>();
     let lut = &LUT;
 
@@ -73,20 +112,28 @@ unsafe fn inner1(s: &[u8]) -> u32 {
         };
     }
 
-    for _ in 0..512 {
-        let chunk = ptr.read_unaligned();
-        let chunk = chunk - Simd::splat(b'0' as _);
-        let mask = chunk.simd_lt(Simd::splat(0)).to_bitmask() as u32;
-        let step = _pdep_u32(8, mask).trailing_zeros() + 1;
-        let shuffle = lut.as_ptr().byte_add(((mask & 0x7FC) * 4) as usize).read();
-        let chunk = _mm_shuffle_epi8(chunk.into(), shuffle.into());
+    for _ in 0..256 {
+        macro_rules! half_chunk {
+            () => {{
+                let chunk = ptr.read_unaligned();
+                let chunk = chunk - Simd::splat(b'0' as _);
+                let mask = chunk.simd_lt(Simd::splat(0)).to_bitmask() as u32;
+                let step = _pdep_u32(8, mask).trailing_zeros() + 1;
+                let shuffle = lut.as_ptr().byte_add(((mask & 0x7FC) * 4) as usize).read();
+                let chunk = _mm_shuffle_epi8(chunk.into(), shuffle.into());
+                ptr = ptr.byte_add(step as usize);
+                chunk
+            }};
+        }
+        let a = half_chunk!();
+        let b = half_chunk!();
+        let chunk = _mm_packus_epi16(a, b);
         let chunk = _mm_maddubs_epi16(chunk, u16x8::splat(u16::from_ne_bytes([10, 1])).into());
         let chunk: u32x4 = _mm_madd_epi16(chunk, u16x8::from_array([72, 1, 72, 1, 72, 1, 72, 1]).into()).into();
-        let p1 = chunk[0];
-        let p2 = chunk[1];
-        bts!(p1);
-        bts!(p2);
-        ptr = ptr.byte_add(step as usize);
+        bts!(chunk[0]);
+        bts!(chunk[1]);
+        bts!(chunk[2]);
+        bts!(chunk[3]);
     }
 
     static mut FRONT: [u16; 256] = [0; 256];
