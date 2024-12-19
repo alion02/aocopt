1fc14cc154997cd61a27b15318986d49bc01b80c
alion02 <alojczek@gmail.com>
2024-12-19 09:57:57 +0100

    Prepare day 19

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,19 +2,19 @@
 
 use std::{fs::read_to_string, hint::black_box};
 
-use aocopt::day18::{part1, part2};
+use aocopt::day19::{part1, part2};
 use criterion::{criterion_group, criterion_main, Criterion};
 
-pub fn day18(c: &mut Criterion) {
-    let s = read_to_string("./inputs/18.txt").unwrap();
+pub fn day19(c: &mut Criterion) {
+    let s = read_to_string("./inputs/19.txt").unwrap();
     let s = s.as_str();
 
-    c.bench_function("day18 part1", |b| b.iter(|| part1(black_box(s))));
-    c.bench_function("day18 part2", |b| b.iter(|| part2(black_box(s))));
+    c.bench_function("day19 part1", |b| b.iter(|| part1(black_box(s))));
+    // c.bench_function("day19 part2", |b| b.iter(|| part2(black_box(s))));
 
-    assert_eq!(part1(s).to_string(), read_to_string("./outputs/18p1.txt").unwrap());
-    assert_eq!(part2(s).to_string(), read_to_string("./outputs/18p2.txt").unwrap());
+    assert_eq!(part1(s).to_string(), read_to_string("./outputs/19p1.txt").unwrap());
+    // assert_eq!(part2(s).to_string(), read_to_string("./outputs/19p2.txt").unwrap());
 }
 
-criterion_group!(benches, day18);
+criterion_group!(benches, day19);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -50,6 +50,8 @@ macro_rules! black_box {
 extern crate aoc_runner_derive;
 extern crate aoc_runner;
 
+pub mod day19;
+
 pub mod day18;
 
 // pub mod day17;
