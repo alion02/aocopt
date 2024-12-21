2d4c23cb1617fb28a040be61feeda0732a0eaf40
alion02 <alojczek@gmail.com>
2024-12-21 05:59:34 +0100

    initial p2

--- benches/bench.rs
+++ benches/bench.rs
@@ -10,10 +10,10 @@ pub fn day20(c: &mut Criterion) {
     let s = s.as_str();
 
     c.bench_function("day20 part1", |b| b.iter(|| part1(black_box(s))));
-    // c.bench_function("day20 part2", |b| b.iter(|| part2(black_box(s))));
+    c.bench_function("day20 part2", |b| b.iter(|| part2(black_box(s))));
 
     assert_eq!(part1(s).to_string(), read_to_string("./outputs/20p1.txt").unwrap());
-    // assert_eq!(part2(s).to_string(), read_to_string("./outputs/20p2.txt").unwrap());
+    assert_eq!(part2(s).to_string(), read_to_string("./outputs/20p2.txt").unwrap());
 }
 
 criterion_group!(benches, day20);
