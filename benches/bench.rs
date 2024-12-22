90a4bf07798dbaec6d4bc1f23632a083ae3faccf
alion02 <alojczek@gmail.com>
2024-12-22 05:04:14 +0100

    try to fix the bench

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,8 +9,18 @@ pub fn day21(c: &mut Criterion) {
     let s = read_to_string("./inputs/21.txt").unwrap();
     let s = s.as_str();
 
-    c.bench_function("day21 part1", |b| b.iter(|| part1(black_box(s))));
-    c.bench_function("day21 part2", |b| b.iter(|| part2(black_box(s))));
+    c.bench_function("day21 part1", |b| {
+        b.iter(
+            #[inline(never)]
+            || part1(black_box(s)),
+        )
+    });
+    c.bench_function("day21 part2", |b| {
+        b.iter(
+            #[inline(never)]
+            || part2(black_box(s)),
+        )
+    });
 
     assert_eq!(part1(s).to_string(), read_to_string("./outputs/21p1.txt").unwrap());
     assert_eq!(part2(s).to_string(), read_to_string("./outputs/21p2.txt").unwrap());
