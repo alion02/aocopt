793d67dfb5955a013ed1f43fcfa3a48817b5cf6b
alion02 <alojczek@gmail.com>
2024-12-22 09:47:04 +0100

    Prepare day 22

--- benches/bench.rs
+++ benches/bench.rs
@@ -2,29 +2,29 @@
 
 use std::{fs::read_to_string, hint::black_box};
 
-use aocopt::day21::{part1, part2};
+use aocopt::day22::{part1, part2};
 use criterion::{criterion_group, criterion_main, Criterion};
 
-pub fn day21(c: &mut Criterion) {
-    let s = read_to_string("./inputs/21.txt").unwrap();
+pub fn day22(c: &mut Criterion) {
+    let s = read_to_string("./inputs/22.txt").unwrap();
     let s = s.as_str();
 
-    c.bench_function("day21 part1", |b| {
+    c.bench_function("day22 part1", |b| {
         b.iter(
             #[inline(never)]
             || part1(black_box(s)),
         )
     });
-    c.bench_function("day21 part2", |b| {
+    c.bench_function("day22 part2", |b| {
         b.iter(
             #[inline(never)]
             || part2(black_box(s)),
         )
     });
 
-    assert_eq!(part1(s).to_string(), read_to_string("./outputs/21p1.txt").unwrap());
-    assert_eq!(part2(s).to_string(), read_to_string("./outputs/21p2.txt").unwrap());
+    assert_eq!(part1(s).to_string(), read_to_string("./outputs/22p1.txt").unwrap());
+    assert_eq!(part2(s).to_string(), read_to_string("./outputs/22p2.txt").unwrap());
 }
 
-criterion_group!(benches, day21);
+criterion_group!(benches, day22);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -14,7 +14,15 @@
 
 #[allow(unused)]
 use std::{
-    arch::{asm, x86_64::*},
+    arch::{
+        asm,
+        x86_64::{
+            __m128i, __m256i, _bextr2_u32, _mm256_madd_epi16, _mm256_maddubs_epi16, _mm256_movemask_epi8,
+            _mm256_shuffle_epi8, _mm_hadd_epi16, _mm_madd_epi16, _mm_maddubs_epi16, _mm_minpos_epu16,
+            _mm_movemask_epi8, _mm_packus_epi16, _mm_packus_epi32, _mm_shuffle_epi8, _mm_testc_si128, _pdep_u32,
+            _pext_u32, _pext_u64,
+        },
+    },
     array,
     fmt::Display,
     hint::assert_unchecked,
@@ -45,7 +53,9 @@ macro_rules! black_box {
 extern crate aoc_runner_derive;
 extern crate aoc_runner;
 
-pub mod day21;
+pub mod day22;
+
+// pub mod day21;
 
 // pub mod day20;
 
