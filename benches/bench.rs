477f5c909d0359d2f8e589272fa4d511aab5618e
alion02 <alojczek@gmail.com>
2024-12-23 09:18:38 +0100

    and bench it

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,12 +9,12 @@ pub fn day22(c: &mut Criterion) {
     let s = read_to_string("./inputs/22.txt").unwrap();
     let s = s.as_str();
 
-    // c.bench_function("day22 part1", |b| {
-    //     b.iter(
-    //         #[inline(never)]
-    //         || part1(black_box(s)),
-    //     )
-    // });
+    c.bench_function("day22 part1", |b| {
+        b.iter(
+            #[inline(never)]
+            || part1(black_box(s)),
+        )
+    });
     c.bench_function("day22 part2", |b| {
         b.iter(
             #[inline(never)]
@@ -22,7 +22,7 @@ pub fn day22(c: &mut Criterion) {
         )
     });
 
-    // assert_eq!(part1(s).to_string(), read_to_string("./outputs/22p1.txt").unwrap());
+    assert_eq!(part1(s).to_string(), read_to_string("./outputs/22p1.txt").unwrap());
     assert_eq!(part2(s).to_string(), read_to_string("./outputs/22p2.txt").unwrap());
 }
 
