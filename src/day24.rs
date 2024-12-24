ad75c7ef5eb43c1163d7d43304cfe6efba627a6d
alion02 <alojczek@gmail.com>
2024-12-25 00:20:25 +0100

    initial solve p1

--- src/day24.rs
+++ src/day24.rs
@@ -3,7 +3,139 @@ use super::*;
 #[inline]
 #[repr(align(64))]
 unsafe fn inner1(s: &[u8]) -> u64 {
-    0
+    let r = s.as_ptr_range();
+    let mut ptr = r.start;
+    #[repr(C, align(8))]
+    #[derive(Clone, Copy)]
+    struct Node {
+        a: u16,
+        ctrl: u8,
+        _padding1: u8,
+        b: u16,
+        _padding2: u16,
+    }
+    static mut GRAPH: [Node; 36 * 36 * 36] = [Node {
+        a: 0,
+        ctrl: 0,
+        _padding1: 0,
+        b: 0,
+        _padding2: 0,
+    }; 36 * 36 * 36];
+    let graph = &mut GRAPH;
+    for c1 in b'x'..b'y' + 1 {
+        for c2 in b'0'..b'4' + 1 {
+            for c3 in b'0'..if c2 == b'4' { b'4' } else { b'9' } + 1 {
+                let idx = (c1 - b'a' + 10) as u32 * 36 * 36 + (c2 - b'0') as u32 * 36 + (c3 - b'0') as u32;
+                graph.get_unchecked_mut(idx as usize).ctrl = *ptr.add(5) & 1;
+                ptr = ptr.add(7);
+            }
+        }
+    }
+
+    asm!(
+    "20:",
+        "cmp byte ptr[{ptr} + 5], {ascii_O}",
+        "jne 21f",
+        // OR
+        "vmovdqu {chunk}, [{ptr}]",
+        "vpblendw {chunk}, {chunk}, [{ptr} + 2], 0xC0",
+        "vpsubb {tmp}, {chunk}, {vec_ascii_am10}",
+        "vpsubb {chunk}, {chunk}, {vec_ascii_0}",
+        "vpblendvb {chunk}, {tmp}, {chunk}, {tmp}",
+        "vpmaddubsw {chunk}, {mults_short}, {chunk}",
+        "vpmaddwd {chunk}, {chunk}, {mults_wide}",
+        "vpextrd {idx:e}, {chunk}, 3",
+        "vpackusdw {chunk}, {chunk}, {chunk}",
+        "vmovq [{graph} + {idx} * 8], {chunk}",
+        "mov byte ptr[{graph} + {idx} * 8 + 2], 3",
+        "add {ptr}, 18",
+        "cmp {ptr}, {end}",
+        "jne 20b",
+        "jmp 40f",
+    "21:",
+        "jl 22f",
+        // XOR
+        "vmovdqu {chunk}, [{ptr}]",
+        "vpblendw {chunk}, {chunk}, [{ptr} + 3], 0xC0",
+        "vpsubb {tmp}, {chunk}, {vec_ascii_am10}",
+        "vpsubb {chunk}, {chunk}, {vec_ascii_0}",
+        "vpblendvb {chunk}, {tmp}, {chunk}, {tmp}",
+        "vpmaddubsw {chunk}, {mults_long}, {chunk}",
+        "vpmaddwd {chunk}, {chunk}, {mults_wide}",
+        "vpextrd {idx:e}, {chunk}, 3",
+        "vpackusdw {chunk}, {chunk}, {chunk}",
+        "vmovq [{graph} + {idx} * 8], {chunk}",
+        "mov byte ptr[{graph} + {idx} * 8 + 2], 4",
+        "add {ptr}, 19",
+        "cmp {ptr}, {end}",
+        "jne 20b",
+        "jmp 40f",
+    "22:",
+        // AND
+        "vmovdqu {chunk}, [{ptr}]",
+        "vpblendw {chunk}, {chunk}, [{ptr} + 3], 0xC0",
+        "vpsubb {tmp}, {chunk}, {vec_ascii_am10}",
+        "vpsubb {chunk}, {chunk}, {vec_ascii_0}",
+        "vpblendvb {chunk}, {tmp}, {chunk}, {tmp}",
+        "vpmaddubsw {chunk}, {mults_long}, {chunk}",
+        "vpmaddwd {chunk}, {chunk}, {mults_wide}",
+        "vpextrd {idx:e}, {chunk}, 3",
+        "vpackusdw {chunk}, {chunk}, {chunk}",
+        "vmovq [{graph} + {idx} * 8], {chunk}",
+        "mov byte ptr[{graph} + {idx} * 8 + 2], 2",
+        "add {ptr}, 19",
+        "cmp {ptr}, {end}",
+        "jne 20b",
+    "40:",
+
+        chunk = out(xmm_reg) _,
+        tmp = out(xmm_reg) _,
+        vec_ascii_am10 = in(xmm_reg) u8x16::splat(b'a' - 10),
+        vec_ascii_0 = in(xmm_reg) u8x16::splat(b'0'),
+        mults_short = in(xmm_reg) u8x16::from_array([0, 36, 36, 1, 0, 0, 0, 0, 36, 1, 1, 0, 0, 36, 36, 1]),
+        mults_long = in(xmm_reg) u8x16::from_array([0, 36, 36, 1, 0, 0, 0, 0, 0, 36, 36, 1, 0, 36, 36, 1]),
+        mults_wide = in(xmm_reg) u16x8::from_array([36, 1, 36, 1, 36, 1, 36, 1]),
+        ptr = inout(reg) ptr => _,
+        idx = out(reg) _,
+        ascii_O = const b'O',
+        graph = in(reg) graph,
+        end = in(reg) r.end.sub(1),
+
+        options(nostack),
+    );
+
+    let mut res = 0;
+    let c1 = b'z';
+    for c2 in (b'0'..b'4' + 1).rev() {
+        for c3 in (b'0'..if c2 == b'4' { b'5' } else { b'9' } + 1).rev() {
+            let idx = (c1 - b'a' + 10) as u32 * 36 * 36 + (c2 - b'0') as u32 * 36 + (c3 - b'0') as u32;
+            unsafe fn value_of(graph: &mut [Node; 36 * 36 * 36], idx: usize) -> u64 {
+                let &mut Node {
+                    a: a_1,
+                    ctrl,
+                    _padding1,
+                    b: b_1,
+                    _padding2,
+                } = graph.get_unchecked_mut(idx);
+                if ctrl < 2 {
+                    ctrl as u64
+                } else {
+                    let a = value_of(graph, a_1 as usize);
+                    let b = value_of(graph, b_1 as usize);
+                    let res = match ctrl.cmp(&3) {
+                        std::cmp::Ordering::Less => a & b,
+                        std::cmp::Ordering::Equal => a | b,
+                        std::cmp::Ordering::Greater => a ^ b,
+                    };
+                    graph.get_unchecked_mut(idx).ctrl = res as u8;
+                    res
+                }
+            }
+            res <<= 1;
+            res |= value_of(graph, idx as usize);
+        }
+    }
+    res
 }
 
 #[inline]
